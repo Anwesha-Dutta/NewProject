8b081d5ef3812429aefe0b1975d7e42ca1261212
Anwesha-Dutta <anweshadutta891@gmail.com>
2023-09-26 03:01:31 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
